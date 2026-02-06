22dbe84a9810f408458a0e15a4a8269b213c926c
Miotis2004 <miotis2004@gmail.com>
2026-02-06 12:56:21 -0800

    Fix preset file sorting comparator

--- RonnieAmpModeler/Source/UI/MainLayout.h
+++ RonnieAmpModeler/Source/UI/MainLayout.h
@@ -43,7 +43,7 @@ public:
             "Save Preset",
             "Enter a preset name:",
             juce::AlertWindow::NoIcon);
-        presetNamePrompt->addTextEditor("presetName");
+        presetNamePrompt->addTextEditor("presetName", "");
         presetNamePrompt->addButton("Save", 1, juce::KeyPress(juce::KeyPress::returnKey));
         presetNamePrompt->addButton("Cancel", 0, juce::KeyPress(juce::KeyPress::escapeKey));
         presetNamePrompt->enterModalState(true, juce::ModalCallbackFunction::create([this](int result)
@@ -104,10 +104,19 @@ private:
     {
         presetList.clear();
         presetFiles = presetManager.getPresetFiles();
-        presetFiles.sort([](const juce::File& a, const juce::File& b)
+        struct PresetFileSorter
         {
-            return a.getLastModificationTime() > b.getLastModificationTime();
-        });
+            static int compareElements(const juce::File& first, const juce::File& second)
+            {
+                if (first.getLastModificationTime() > second.getLastModificationTime())
+                    return -1;
+                if (first.getLastModificationTime() < second.getLastModificationTime())
+                    return 1;
+                return 0;
+            }
+        };
+
+        presetFiles.sort(PresetFileSorter{});
 
         for (int i = 0; i < presetFiles.size(); ++i)
         {
